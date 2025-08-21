8a8be45ae039493ea68885d5fa5a20f774527118
Chris Jones <cmsj@tenshu.net>
2025-08-21 22:50:10 +0100

    Stage some changes

--- update_changelog.sh
+++ update_changelog.sh
@@ -15,6 +15,12 @@ BASEIMG=$(cat "${BASE_DATA}" | awk '{ print $3 }')
 BINDVER=$(cat "${DEPS_DATA}" | grep " bind " | sed -e 's/.*(\(.*\))/\1/')
 DEPS=$(cat "${DEPS_DATA}" | grep -v " bind " | sed -e 's/.*Installing / \* /g' | sort)
 
+OLD_BINDVER=$(cat "${CHANGELOG}" | grep "BIND version" | sed -e 's/^ \* BIND version: //')
+OLD_BASEVER=$(cat "${CHANGELOG}" | grep "base image version" | sed -e 's/^.* image version: //')
+OLD_DEPS=$(cat "${CHANGELOG}" | sed '0,/^Dependencies:$/d')
+
+echo "Old: ${OLD_BINDVER} New ${BINDVER}"
+
 echo -n "" >"${CHANGELOG}"
 {
     if [ -e "${BREAKING}" ]; then
@@ -27,10 +33,24 @@ echo -n "" >"${CHANGELOG}"
     echo "# Version information"
     echo ""
     echo " * Addon version: $(cat "${ADDON_DATA}")"
-    echo " * BIND version: ${BINDVER}"
-    echo " * Home Assistant Add-on base image version: ${BASEIMG}"
+
+    echo -n " * BIND version: "
+    if [ ${OLD_BINDVER} != ${BINDVER} ]; then
+        echo "${OLD_BINDVER} -> ${BINDVER}"
+    else
+        echo "${BINDVER} (unchanged)"
+    fi
+
+    echo -n " * Home Assistant Add-on base image version: "
+    if [ "${OLD_BASEVER}" != "${BASEIMG}" ]; then
+        echo "${OLD_BASEVER} -> ${BASEIMG}"
+    else
+        echo "${BASEIMG} (unchanged)"
+    fi
+
     echo ""
     echo "Dependencies:"
+
     echo "${DEPS}"
 } >> "${CHANGELOG}"
 
